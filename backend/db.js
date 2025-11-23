25fc5ad18340659ce7a9bf23793c4ebc6c7633ed
shreenithya6 <shreenithyamarrikanti2306@gmail.com>
2025-11-23 13:44:36 +0530

    minor changes

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,8 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
 dotenv.config();
 const connectDB=async()=>{
     try{
-        console.log("MONGO_URI =", process.env.MONGO_URI);
          await mongoose.connect(process.env.MONGO_URI);
          console.log("Mongodb connected");
     }
@@ -11,4 +10,4 @@ const connectDB=async()=>{
         console.log("Mongodb failed to connect");
     }
 }
-export default connectDB;
+module.exports=connectDB;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ connectDB();
 const User = require('./models/User.js');
 const bcrypt = require('bcrypt');
 const app = express();
+require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
@@ -20,3 +21,8 @@ app.post('/register', async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
+});
