a3186b673bbbbf001e44461b48e730f7143fa7fe
shreenithya6 <shreenithyamarrikanti2306@gmail.com>
2025-11-23 13:27:25 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 connectDB();
-// const dotenv = require('dotenv');
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-// dotenv.config();
-
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-
-// const start = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI);
-//         console.log("MongoDB Connected ✔️");
-
-//         const PORT = process.env.PORT || 5000;
-//         app.listen(PORT, () => {
-//             console.log(`Server running on port ${PORT} 🚀`);
-//         });
-//     } catch (err) {
-//         console.error("MongoDB Connection Error:", err);
-//     }
-// };
-
-// start();
