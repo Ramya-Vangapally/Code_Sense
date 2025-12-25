1ce6b3573dbbfe89aba387205b50ffc79e64505e
Ramya-Vangapally <vangapallyramyaa@gmail.com>
2025-12-25 19:58:07 +0530

    deploy 14

--- FrontEnd/admin_email.js
+++ FrontEnd/admin_email.js
@@ -1,7 +1,7 @@
 (function(){
   const API_BASE = window.location.hostname === "127.0.0.1"
     ? "http://127.0.0.1:5000"
-    : "http://localhost:5000";
+    : "https://code-sense-bxqi.onrender.com";
 
   const INITIAL_HISTORY_COUNT = 5;
   const HISTORY_INCREMENT = 10;
